9e5db4c7838d1cb883d2f42a427ba918970e9db2
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-04-15 22:53:50 +0800

    feat: add the basic definition for the lexer

---
+++ src/frontend/lexeme/lexer/VnlLexer.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __VNL_LEXER_HPP__
+#define __VNL_LEXER_HPP__
+
+#include "../token/VnlToken.hpp"
+
+#include <istream>
+
+class VnlLexer {
+private:
+    std::istream source;
+    int currentCharIndex;
+
+    int line;
+    int column;
+
+    void advance();
+
+public:
+    VnlLexer(std::istream& input);
+
+    VnlToken next();
+};
+
+#endif // __VNL_LEXER_HPP__
