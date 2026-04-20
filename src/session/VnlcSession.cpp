d03b113431cf4e54779fafcec3b8030564e173db
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-04-20 17:26:17 +0800

    chore: add todo for session implementation

--- src/session/VnlcSession.cpp
+++ src/session/VnlcSession.cpp
@@ -1,7 +1,10 @@
 #include "VnlcSession.hpp"
+#include "../log/VnlcLogger.hpp"
 
 VnlcSession::VnlcSession(VnlcConfig&& config) : config(config) {}
 
 void VnlcSession::run() {
-    // TODO
+    VNLC_LOG_INFO("Session started.");
+
+    // TODO: Implement the main logic of the session, including lexing, parsing, semantic analysis, optimization and code generation.
 }
