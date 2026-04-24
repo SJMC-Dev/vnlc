b5ad7cf7a404453adcf54d971405437540da6cc5
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-04-24 17:34:22 +0800

    feat: add public methods for parser

--- src/parser/VnlcParser.hpp
+++ src/parser/VnlcParser.hpp
@@ -3,13 +3,21 @@
 #ifndef VNLC_PARSER_HPP
 #define VNLC_PARSER_HPP
 
+#include "../ast/VnlcAstNode.hpp"
 #include "../lexer/VnlcLexer.hpp"
 #include "../token/VnlcToken.hpp"
+#include <memory>
 
 class VnlcParser {
 private:
     VnlcLexer lexer;
     VnlcToken currentToken;
+
+public:
+    VnlcParser(VnlcLexer&& lexer);
+
+    bool hasNext() const;
+    std::unique_ptr<VnlcAstNode> parse();
 };
 
 #endif // VNLC_PARSER_HPP
