21f40b4e8ab57bb77b0280e97d935fe6f66535be
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-04-24 17:01:53 +0800

    feat: add VnlcParser class and its private members

---
+++ src/parser/VnlcParser.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef VNLC_PARSER_HPP
+#define VNLC_PARSER_HPP
+
+#include "../lexer/VnlcLexer.hpp"
+
+class VnlcParser {
+private:
+    VnlcLexer lexer;
+};
+
+#endif // VNLC_PARSER_HPP
