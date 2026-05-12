44bb5e3d64c82dad8202e3cafaeaa80391bdc9b0
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-05-12 18:03:32 +0800

    feat: update parsing methods to specify return types and part of contexts

--- src/parser/VnlcParser.hpp
+++ src/parser/VnlcParser.hpp
@@ -3,56 +3,14 @@
 #ifndef VNLC_PARSER_HPP
 #define VNLC_PARSER_HPP
 
-#include "../ast/declaration/VnlcClassDeclarationNode.hpp"
-#include "../ast/declaration/VnlcDeclarationItem.hpp"
-#include "../ast/declaration/VnlcDeclarationNode.hpp"
-#include "../ast/declaration/VnlcEnumDeclarationNode.hpp"
-#include "../ast/declaration/VnlcEnumMemberDeclarationNode.hpp"
-#include "../ast/declaration/VnlcExportDeclarationItem.hpp"
-#include "../ast/declaration/VnlcExportDeclarationNode.hpp"
-#include "../ast/declaration/VnlcFunctionDeclarationNode.hpp"
-#include "../ast/declaration/VnlcImportDeclarationItem.hpp"
-#include "../ast/declaration/VnlcImportDeclarationNode.hpp"
-#include "../ast/declaration/VnlcInterfaceDeclarationNode.hpp"
-#include "../ast/declaration/VnlcPropertyDeclarationNode.hpp"
-#include "../ast/declaration/VnlcTypeAliasDeclarationNode.hpp"
-#include "../ast/declaration/VnlcTypeDeclarationNode.hpp"
-#include "../ast/declaration/VnlcVariableDeclarationNode.hpp"
-#include "../ast/expression/VnlcBinaryExpressionNode.hpp"
-#include "../ast/expression/VnlcConditionalExpressionNode.hpp"
-#include "../ast/expression/VnlcDictLiteralExpressionNode.hpp"
-#include "../ast/expression/VnlcExpressionNode.hpp"
-#include "../ast/expression/VnlcListLikeLiteralExpressionNode.hpp"
-#include "../ast/expression/VnlcLiteralExpressionNode.hpp"
-#include "../ast/expression/VnlcRangeExpressionNode.hpp"
-#include "../ast/expression/VnlcSelectorLiteralExpressionNode.hpp"
-#include "../ast/expression/VnlcSimpleLiteralExpressionNode.hpp"
-#include "../ast/expression/VnlcStringLiteralExpressionNode.hpp"
-#include "../ast/expression/VnlcUnaryExpressionNode.hpp"
 #include "../ast/module/VnlcModuleNode.hpp"
-#include "../ast/other/VnlcTypeAnnotationNode.hpp"
-#include "../ast/other/VnlcTypeNode.hpp"
-#include "../ast/statement/VnlcBlockStatementNode.hpp"
-#include "../ast/statement/VnlcBreakStatementNode.hpp"
-#include "../ast/statement/VnlcContinueStatementNode.hpp"
-#include "../ast/statement/VnlcControlFlowStatementNode.hpp"
-#include "../ast/statement/VnlcExpressionStatementNode.hpp"
-#include "../ast/statement/VnlcForStatementNode.hpp"
-#include "../ast/statement/VnlcIfStatementNode.hpp"
-#include "../ast/statement/VnlcReloadStatementNode.hpp"
-#include "../ast/statement/VnlcReturnStatementNode.hpp"
-#include "../ast/statement/VnlcStatementNode.hpp"
-#include "../ast/statement/VnlcSwitchStatementItem.hpp"
-#include "../ast/statement/VnlcSwitchStatementNode.hpp"
-#include "../ast/statement/VnlcVariableDeclarationStatementNode.hpp"
-#include "../ast/statement/VnlcWhileStatementNode.hpp"
 #include "../config/VnlcConfig.hpp"
 #include "../lexer/VnlcLexer.hpp"
 #include "../token/VnlcToken.hpp"
+#include "inherited/VnlcParsingContexts.hpp" // IWYU pragma: keep
+#include "synthesized/VnlcParsingResults.hpp" // IWYU pragma: keep
 #include <memory>
 #include <span>
-#include <utility>
-#include <variant>
 #include <vector>
 
 class VnlcParser {
@@ -71,94 +29,96 @@ private:
     [[nodiscard]] bool match(std::span<VnlcTokenType> expectedTypes) const;
 
     // TODO: specify parameters and return types for each parsing function, use void before we determine them
-    [[nodiscard]] std::unique_ptr<VnlcModuleNode> parseModule(const VnlcConfig& config);
-    [[nodiscard]] std::unique_ptr<VnlcDeclarationNode> parseTopIdentifierDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcImportDeclarationNode> parseImportDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcExportDeclarationNode> parseExportDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcVariableDeclarationNode> parseVariableDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcFunctionDeclarationNode> parseFunctionDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcTypeDeclarationNode> parseTypeDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcPropertyDeclarationNode> parsePropertyDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcFunctionDeclarationNode> parseClassMethodDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcFunctionDeclarationNode> parseInterfaceMethodDeclaration();
-    [[nodiscard]] std::vector<VnlcDeclarationItem::MetadataTerm> parseMetadata();
-    [[nodiscard]] std::unique_ptr<VnlcFunctionDeclarationNode> parseRegularFunctionDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcFunctionDeclarationNode> parseNativeFunctionDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcTypeAnnotationNode> parseTypeAnnotation();
-    [[nodiscard]] std::vector<std::pair<std::string, std::unique_ptr<VnlcTypeAnnotationNode>>> parseParameterList();
-    [[nodiscard]] std::unique_ptr<VnlcClassDeclarationNode> parseClassDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcInterfaceDeclarationNode> parseInterfaceDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcEnumDeclarationNode> parseEnumDeclaration();
-    [[nodiscard]] std::unique_ptr<VnlcTypeAliasDeclarationNode> parseTypeAliasDeclaration();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseImportPath();
-    [[nodiscard]] std::vector<VnlcExportDeclarationItem> parseExportList();
-    [[nodiscard]] VnlcDeclarationItem::MetadataTerm parseMetadataTerm();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseAbsoluteImportPath();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseRelativeImportPath();
-    [[nodiscard]] std::unique_ptr<VnlcTypeNode> parseType();
-    [[nodiscard]] std::pair<std::string, std::unique_ptr<VnlcTypeAnnotationNode>> parseParameter();
-    [[nodiscard]] std::vector<std::string> parseGenericParameterList();
-    [[nodiscard]] std::vector<std::unique_ptr<VnlcTypeNode>> parseGenericArgumentList();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseAbsoluteImportPathList();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseRelativeImportPathList();
-    [[nodiscard]] std::unique_ptr<VnlcBlockStatementNode> parseFunctionBody();
-    [[nodiscard]] std::vector<std::unique_ptr<VnlcDeclarationNode>> parseClassBody();
-    [[nodiscard]] std::vector<std::unique_ptr<VnlcFunctionDeclarationNode>> parseInterfaceBody();
-    [[nodiscard]] std::vector<std::unique_ptr<VnlcEnumMemberDeclarationNode>> parseEnumBody();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseAbsoluteImportPathItem();
-    [[nodiscard]] std::vector<VnlcImportDeclarationItem> parseRelativeImportPathItem();
-    [[nodiscard]] std::unique_ptr<VnlcDeclarationNode> parseClassMember();
-    [[nodiscard]] std::unique_ptr<VnlcEnumMemberDeclarationNode> parseEnumMemberDeclaration();
-    [[nodiscard]] std::vector<std::pair<std::string, std::unique_ptr<VnlcTypeAnnotationNode>>> parseEnumAssociatedValueList();
-    [[nodiscard]] std::pair<std::string, std::unique_ptr<VnlcTypeAnnotationNode>> parseEnumAssociatedValue();
-    [[nodiscard]] std::unique_ptr<VnlcExpressionNode> parseExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseAssignmentExpression();
-    [[nodiscard]] std::unique_ptr<VnlcConditionalExpressionNode> parseConditionalExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseNullishCoalescingExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseLogicalOrExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseLogicalAndExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseEqualityExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseRelationalExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseBitwiseOrExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseBitwiseXorExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseBitwiseAndExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseShiftExpression();
-    [[nodiscard]] std::unique_ptr<VnlcRangeExpressionNode> parseRangeExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseAdditiveExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseMultiplicativeExpression();
-    [[nodiscard]] std::unique_ptr<VnlcUnaryExpressionNode> parseUnaryExpression();
-    [[nodiscard]] std::unique_ptr<VnlcBinaryExpressionNode> parseExponentialExpression();
-    [[nodiscard]] std::unique_ptr<VnlcExpressionNode> parsePostfixExpression();
-    [[nodiscard]] std::unique_ptr<VnlcExpressionNode> parsePrimaryExpression();
-    [[nodiscard]] VnlcToken parseAssignmentOperator();
-    [[nodiscard]] std::unique_ptr<VnlcExpressionNode> parsePostfixSuffix();
-    [[nodiscard]] std::unique_ptr<VnlcLiteralExpressionNode> parseLiteral();
-    [[nodiscard]] std::unique_ptr<VnlcStringLiteralExpressionNode> parseString();
-    [[nodiscard]] std::unique_ptr<VnlcSimpleLiteralExpressionNode> parseBoolean();
-    [[nodiscard]] std::unique_ptr<VnlcListLikeLiteralExpressionNode> parseListLiteral();
-    [[nodiscard]] std::unique_ptr<VnlcDictLiteralExpressionNode> parseDictLiteral();
-    [[nodiscard]] std::unique_ptr<VnlcListLikeLiteralExpressionNode> parseSNBTArray();
-    [[nodiscard]] std::unique_ptr<VnlcSelectorLiteralExpressionNode> parseSelector();
-    [[nodiscard]] std::vector<std::unique_ptr<VnlcExpressionNode>> parseArgumentList();
-    [[nodiscard]] std::unique_ptr<VnlcExpressionNode> parseInterpolation();
-    [[nodiscard]] std::variant<std::string, std::unique_ptr<VnlcExpressionNode>> parseStringPart();
-    [[nodiscard]] std::pair<std::string, std::unique_ptr<VnlcExpressionNode>> parseDictEntry();
-    [[nodiscard]] std::unordered_map<std::string, std::unique_ptr<VnlcExpressionNode>> parseSelectorArgumentList();
-    [[nodiscard]] std::pair<std::string, std::unique_ptr<VnlcExpressionNode>> parseSelectorArgument();
-    [[nodiscard]] std::unique_ptr<VnlcStatementNode> parseStatement();
-    [[nodiscard]] std::unique_ptr<VnlcExpressionStatementNode> parseExpressionStatement();
-    [[nodiscard]] std::unique_ptr<VnlcVariableDeclarationStatementNode> parseVariableDeclarationStatement();
-    [[nodiscard]] std::unique_ptr<VnlcBlockStatementNode> parseBlockStatement();
-    [[nodiscard]] std::unique_ptr<VnlcControlFlowStatementNode> parseControlFlowStatement();
-    [[nodiscard]] std::unique_ptr<VnlcIfStatementNode> parseIfStatement();
-    [[nodiscard]] std::unique_ptr<VnlcSwitchStatementNode> parseSwitchStatement();
-    [[nodiscard]] std::unique_ptr<VnlcWhileStatementNode> parseWhileStatement();
-    [[nodiscard]] std::unique_ptr<VnlcForStatementNode> parseForStatement();
-    [[nodiscard]] std::unique_ptr<VnlcReturnStatementNode> parseReturnStatement();
-    [[nodiscard]] std::unique_ptr<VnlcBreakStatementNode> parseBreakStatement();
-    [[nodiscard]] std::unique_ptr<VnlcContinueStatementNode> parseContinueStatement();
-    [[nodiscard]] std::unique_ptr<VnlcReloadStatementNode> parseReloadStatement();
-    [[nodiscard]] std::variant<VnlcSwitchStatementItem::LiteralMatchItem, VnlcSwitchStatementItem::TypeMatchItem> parseSwitchCase();
+    [[nodiscard]] VnlcModuleParsingResult parseModule(VnlcModuleParsingContext context);
+    [[nodiscard]] VnlcTopIdentifierDeclarationParsingResult parseTopIdentifierDeclaration();
+    [[nodiscard]] VnlcImportDeclarationParsingResult parseImportDeclaration();
+    [[nodiscard]] VnlcExportDeclarationParsingResult parseExportDeclaration();
+    [[nodiscard]] VnlcVariableDeclarationParsingResult parseVariableDeclaration(VnlcVariableDeclarationParsingContext context);
+    [[nodiscard]] VnlcFunctionDeclarationParsingResult parseFunctionDeclaration();
+    [[nodiscard]] VnlcTypeDeclarationParsingResult parseTypeDeclaration();
+    [[nodiscard]] VnlcPropertyDeclarationParsingResult parsePropertyDeclaration();
+    [[nodiscard]] VnlcClassMethodDeclarationParsingResult parseClassMethodDeclaration();
+    [[nodiscard]] VnlcInterfaceMethodDeclarationParsingResult parseInterfaceMethodDeclaration();
+    [[nodiscard]] VnlcMetadataParsingResult parseMetadata();
+    [[nodiscard]] VnlcVariableDeclarationPrimaryParsingResult parseVariableDeclarationPrimary();
+    [[nodiscard]] VnlcRegularFunctionDeclarationParsingResult parseRegularFunctionDeclaration();
+    [[nodiscard]] VnlcNativeFunctionDeclarationParsingResult parseNativeFunctionDeclaration();
+    [[nodiscard]] VnlcTypeAnnotationParsingResult parseTypeAnnotation();
+    [[nodiscard]] VnlcParameterListParsingResult parseParameterList();
+    [[nodiscard]] VnlcClassDeclarationParsingResult parseClassDeclaration();
+    [[nodiscard]] VnlcInterfaceDeclarationParsingResult parseInterfaceDeclaration();
+    [[nodiscard]] VnlcEnumDeclarationParsingResult parseEnumDeclaration();
+    [[nodiscard]] VnlcTypeAliasDeclarationParsingResult parseTypeAliasDeclaration();
+    [[nodiscard]] VnlcImportPathParsingResult parseImportPath();
+    [[nodiscard]] VnlcExportListParsingResult parseExportList();
+    [[nodiscard]] VnlcMetadataTermParsingResult parseMetadataTerm();
+    [[nodiscard]] VnlcFunctionSignatureParsingResult parseFunctionSignature();
+    [[nodiscard]] VnlcAbsoluteImportPathParsingResult parseAbsoluteImportPath();
+    [[nodiscard]] VnlcRelativeImportPathParsingResult parseRelativeImportPath();
+    [[nodiscard]] VnlcTypeParsingResult parseType();
+    [[nodiscard]] VnlcParameterParsingResult parseParameter();
+    [[nodiscard]] VnlcGenericParameterListParsingResult parseGenericParameterList();
+    [[nodiscard]] VnlcGenericArgumentListParsingResult parseGenericArgumentList();
+    [[nodiscard]] VnlcAbsoluteImportPathListParsingResult parseAbsoluteImportPathList();
+    [[nodiscard]] VnlcRelativeImportPathListParsingResult parseRelativeImportPathList();
+    [[nodiscard]] VnlcFunctionBodyParsingResult parseFunctionBody();
+    [[nodiscard]] VnlcClassBodyParsingResult parseClassBody();
+    [[nodiscard]] VnlcInterfaceBodyParsingResult parseInterfaceBody();
+    [[nodiscard]] VnlcEnumBodyParsingResult parseEnumBody();
+    [[nodiscard]] VnlcAbsoluteImportPathItemParsingResult parseAbsoluteImportPathItem();
+    [[nodiscard]] VnlcRelativeImportPathItemParsingResult parseRelativeImportPathItem();
+    [[nodiscard]] VnlcClassMemberParsingResult parseClassMember();
+    [[nodiscard]] VnlcEnumMemberDeclarationParsingResult parseEnumMemberDeclaration();
+    [[nodiscard]] VnlcEnumAssociatedValueListParsingResult parseEnumAssociatedValueList();
+    [[nodiscard]] VnlcEnumAssociatedValueParsingResult parseEnumAssociatedValue();
+    [[nodiscard]] VnlcExpressionParsingResult parseExpression();
+    [[nodiscard]] VnlcAssignmentExpressionParsingResult parseAssignmentExpression();
+    [[nodiscard]] VnlcConditionalExpressionParsingResult parseConditionalExpression();
+    [[nodiscard]] VnlcNullishCoalescingExpressionParsingResult parseNullishCoalescingExpression();
+    [[nodiscard]] VnlcLogicalOrExpressionParsingResult parseLogicalOrExpression();
+    [[nodiscard]] VnlcLogicalAndExpressionParsingResult parseLogicalAndExpression();
+    [[nodiscard]] VnlcEqualityExpressionParsingResult parseEqualityExpression();
+    [[nodiscard]] VnlcRelationalExpressionParsingResult parseRelationalExpression();
+    [[nodiscard]] VnlcBitwiseOrExpressionParsingResult parseBitwiseOrExpression();
+    [[nodiscard]] VnlcBitwiseXorExpressionParsingResult parseBitwiseXorExpression();
+    [[nodiscard]] VnlcBitwiseAndExpressionParsingResult parseBitwiseAndExpression();
+    [[nodiscard]] VnlcShiftExpressionParsingResult parseShiftExpression();
+    [[nodiscard]] VnlcRangeExpressionParsingResult parseRangeExpression();
+    [[nodiscard]] VnlcAdditiveExpressionParsingResult parseAdditiveExpression();
+    [[nodiscard]] VnlcMultiplicativeExpressionParsingResult parseMultiplicativeExpression();
+    [[nodiscard]] VnlcUnaryExpressionParsingResult parseUnaryExpression();
+    [[nodiscard]] VnlcExponentialExpressionParsingResult parseExponentialExpression();
+    [[nodiscard]] VnlcPostfixExpressionParsingResult parsePostfixExpression();
+    [[nodiscard]] VnlcPrimaryExpressionParsingResult parsePrimaryExpression();
+    [[nodiscard]] VnlcAssignmentOperatorParsingResult parseAssignmentOperator();
+    [[nodiscard]] VnlcPostfixSuffixParsingResult parsePostfixSuffix();
+    [[nodiscard]] VnlcLiteralParsingResult parseLiteral();
+    [[nodiscard]] VnlcStringParsingResult parseString();
+    [[nodiscard]] VnlcBooleanParsingResult parseBoolean();
+    [[nodiscard]] VnlcListLiteralParsingResult parseListLiteral();
+    [[nodiscard]] VnlcDictLiteralParsingResult parseDictLiteral();
+    [[nodiscard]] VnlcSNBTArrayParsingResult parseSNBTArray();
+    [[nodiscard]] VnlcSelectorParsingResult parseSelector();
+    [[nodiscard]] VnlcArgumentListParsingResult parseArgumentList();
+    [[nodiscard]] VnlcInterpolationParsingResult parseInterpolation();
+    [[nodiscard]] VnlcStringPartParsingResult parseStringPart();
+    [[nodiscard]] VnlcDictEntryParsingResult parseDictEntry();
+    [[nodiscard]] VnlcSelectorArgumentListParsingResult parseSelectorArgumentList();
+    [[nodiscard]] VnlcSelectorArgumentParsingResult parseSelectorArgument();
+    [[nodiscard]] VnlcStatementParsingResult parseStatement();
+    [[nodiscard]] VnlcExpressionStatementParsingResult parseExpressionStatement();
+    [[nodiscard]] VnlcVariableDeclarationStatementParsingResult parseVariableDeclarationStatement();
+    [[nodiscard]] VnlcBlockStatementParsingResult parseBlockStatement();
+    [[nodiscard]] VnlcControlFlowStatementParsingResult parseControlFlowStatement();
+    [[nodiscard]] VnlcIfStatementParsingResult parseIfStatement();
+    [[nodiscard]] VnlcSwitchStatementParsingResult parseSwitchStatement();
+    [[nodiscard]] VnlcWhileStatementParsingResult parseWhileStatement();
+    [[nodiscard]] VnlcForStatementParsingResult parseForStatement();
+    [[nodiscard]] VnlcReturnStatementParsingResult parseReturnStatement();
+    [[nodiscard]] VnlcBreakStatementParsingResult parseBreakStatement();
+    [[nodiscard]] VnlcContinueStatementParsingResult parseContinueStatement();
+    [[nodiscard]] VnlcReloadStatementParsingResult parseReloadStatement();
+    [[nodiscard]] VnlcSwitchCaseParsingResult parseSwitchCase();
 
 public:
     explicit VnlcParser(VnlcLexer&& lexer, unsigned int bufferSize = 2);
