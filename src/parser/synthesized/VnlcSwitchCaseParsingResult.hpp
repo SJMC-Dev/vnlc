e27c15af46c1f2e0219acf6d54a63dbcdbfe9827
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-05-24 14:42:49 +0800

    fix: update the definition of the result of parsing switch cases

--- src/parser/synthesized/VnlcSwitchCaseParsingResult.hpp
+++ src/parser/synthesized/VnlcSwitchCaseParsingResult.hpp
@@ -3,14 +3,15 @@
 #ifndef VNLC_SWITCH_CASE_PARSING_RESULT_HPP
 #define VNLC_SWITCH_CASE_PARSING_RESULT_HPP
 
-#include "../../ast/statement/VnlcSwitchStatementItem.hpp"
+#include "../../ast/expression/VnlcLiteralExpressionNode.hpp"
+#include "../../ast/other/VnlcTypeNode.hpp"
 #include "../../ast/statement/VnlcSwitchStatementType.hpp"
 #include <optional>
 
 struct VnlcSwitchCaseParsingResult {
     VnlcSwitchStatementType kind;
-    std::optional<VnlcSwitchStatementItem::LiteralMatchItem> literalMatchItem;
-    std::optional<VnlcSwitchStatementItem::TypeMatchItem> typeMatchItem;
+    std::optional<std::unique_ptr<VnlcLiteralExpressionNode>> literal;
+    std::optional<std::unique_ptr<VnlcTypeNode>> type;
 };
 
 #endif // VNLC_SWITCH_CASE_PARSING_RESULT_HPP
