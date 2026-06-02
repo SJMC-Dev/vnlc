c48006dd280c1b41cef5f62f113492da120d8713
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-06-02 13:18:11 +0800

    feat: update definition for semantic analyzer

--- src/semantic/VnlcSemanticAnalyzer.hpp
+++ src/semantic/VnlcSemanticAnalyzer.hpp
@@ -3,12 +3,42 @@
 #ifndef VNLC_SEMANTIC_ANALYZER_HPP
 #define VNLC_SEMANTIC_ANALYZER_HPP
 
+#include "VnlcSemanticAnalysisResult.hpp"
+#include "VnlcSemanticContext.hpp"
 #include "../ast/module/VnlcModuleNode.hpp"
-#include <memory>
+#include "../ast/declaration/VnlcDeclarationNode.hpp"
+#include "../ast/statement/VnlcStatementNode.hpp"
+#include "../ast/expression/VnlcExpressionNode.hpp"
+#include "../ast/other/VnlcTypeNode.hpp"
 
 class VnlcSemanticAnalyzer {
 private:
-    std::unique_ptr<VnlcModuleNode> module;
+    const VnlcModuleNode* module;
+    VnlcSemanticContext context;
+
+public:
+    explicit VnlcSemanticAnalyzer(const VnlcModuleNode* module);
+
+    VnlcSemanticAnalyzer() = delete;
+    VnlcSemanticAnalyzer(const VnlcSemanticAnalyzer&) = delete;
+    VnlcSemanticAnalyzer& operator=(const VnlcSemanticAnalyzer&) = delete;
+
+    VnlcSemanticAnalyzer(VnlcSemanticAnalyzer&&) noexcept = default;
+    VnlcSemanticAnalyzer& operator=(VnlcSemanticAnalyzer&&) noexcept = default;
+
+    [[nodiscard]] VnlcSemanticAnalysisResult analyze();
+
+private:
+    void collectTopLevelSymbols();
+    void resolveImports();
+    void collectTypeMembers();
+    void checkDeclarations();
+    void checkExports();
+
+    void checkDeclaration(const VnlcDeclarationNode& declaration);
+    void checkStatement(const VnlcStatementNode& statement);
+    void checkExpression(const VnlcExpressionNode& expression);
+    void checkType(const VnlcTypeNode& type);
 };
 
 #endif // VNLC_SEMANTIC_ANALYZER_HPP
