6a435469e98d88ac819a33785276eaf53c356f19
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-06-02 13:17:25 +0800

    feat: define result for semantic analyzer

---
+++ src/semantic/VnlcSemanticAnalysisResult.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef VNLC_SEMANTIC_ANALYSIS_RESULT_HPP
+#define VNLC_SEMANTIC_ANALYSIS_RESULT_HPP
+
+#include "../diagnostic/VnlcDiagnostic.hpp"
+#include <vector>
+
+class VnlcSemanticAnalysisResult {
+private:
+    std::vector<VnlcDiagnostic> diagnostics;
+
+public:
+    VnlcSemanticAnalysisResult();
+    VnlcSemanticAnalysisResult(const VnlcSemanticAnalysisResult&) = default;
+    VnlcSemanticAnalysisResult& operator=(const VnlcSemanticAnalysisResult&) = default;
+    VnlcSemanticAnalysisResult(VnlcSemanticAnalysisResult&&) noexcept = default;
+    VnlcSemanticAnalysisResult& operator=(VnlcSemanticAnalysisResult&&) noexcept = default;
+
+    [[nodiscard]] bool hasErrors() const;
+    [[nodiscard]] bool hasWarnings() const;
+    [[nodiscard]] bool hasNotes() const;
+    [[nodiscard]] const std::vector<VnlcDiagnostic>& getDiagnostics() const;
+};
+
+#endif // VNLC_SEMANTIC_ANALYSIS_RESULT_HPP
