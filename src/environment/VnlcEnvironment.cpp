e3a2c85f8cd93dc68999f8639a922710e204676b
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-07-18 11:49:00 +0800

    fix: remove VnlcEnvironment that may never be used

--- src/environment/VnlcEnvironment.cpp
+++
@@ -1,4 +0,0 @@
-#include "VnlcEnvironment.hpp"
-
-const std::filesystem::path VnlcEnvironment::COMPLILER_CACHE_DIR = std::filesystem::temp_directory_path() / "vnlc_cache";
-const std::filesystem::path VnlcEnvironment::MODULE_INTERFACE_FILE_CACHE_DIR = VnlcEnvironment::COMPLILER_CACHE_DIR / "module_interfaces";
