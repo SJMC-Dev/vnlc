6ccfdf7acb4a872d4982240ca7412d631b6222ce
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-08-18 16:14:14 +0800

    feat: use single instances for primitive types to avoid repeatitive constructions

--- src/semantic/type/VnlcPrimitiveType.cpp
+++ src/semantic/type/VnlcPrimitiveType.cpp
@@ -5,3 +5,68 @@ VnlcPrimitiveType::VnlcPrimitiveType(bool readonly, VnlcPrimitiveTypeKind primit
 VnlcPrimitiveTypeKind VnlcPrimitiveType::getPrimitiveKind() const noexcept {
     return primitiveKind;
 }
+
+const VnlcPrimitiveType* VnlcPrimitiveType::BYTE_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::SHORT_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::INT_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::LONG_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::FLOAT_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::DOUBLE_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::BOOLEAN_TYPE = nullptr;
+const VnlcPrimitiveType* VnlcPrimitiveType::STRING_TYPE = nullptr;
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getByteType() {
+    if (BYTE_TYPE == nullptr) {
+        BYTE_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::BYTE);
+    }
+    return BYTE_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getShortType() {
+    if (SHORT_TYPE == nullptr) {
+        SHORT_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::SHORT);
+    }
+    return SHORT_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getIntType() {
+    if (INT_TYPE == nullptr) {
+        INT_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::INT);
+    }
+    return INT_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getLongType() {
+    if (LONG_TYPE == nullptr) {
+        LONG_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::LONG);
+    }
+    return LONG_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getFloatType() {
+    if (FLOAT_TYPE == nullptr) {
+        FLOAT_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::FLOAT);
+    }
+    return FLOAT_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getDoubleType() {
+    if (DOUBLE_TYPE == nullptr) {
+        DOUBLE_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::DOUBLE);
+    }
+    return DOUBLE_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getBooleanType() {
+    if (BOOLEAN_TYPE == nullptr) {
+        BOOLEAN_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::BOOLEAN);
+    }
+    return BOOLEAN_TYPE;
+}
+
+const VnlcPrimitiveType* VnlcPrimitiveType::getStringType() {
+    if (STRING_TYPE == nullptr) {
+        STRING_TYPE = new VnlcPrimitiveType(false, VnlcPrimitiveTypeKind::STRING);
+    }
+    return STRING_TYPE;
+}
--- src/semantic/type/VnlcPrimitiveType.hpp
+++ src/semantic/type/VnlcPrimitiveType.hpp
@@ -8,10 +8,28 @@ class VnlcPrimitiveType : public VnlcSemanticType {
 private:
     VnlcPrimitiveTypeKind primitiveKind;
 
-public:
+    static const VnlcPrimitiveType* BYTE_TYPE;
+    static const VnlcPrimitiveType* SHORT_TYPE;
+    static const VnlcPrimitiveType* INT_TYPE;
+    static const VnlcPrimitiveType* LONG_TYPE;
+    static const VnlcPrimitiveType* FLOAT_TYPE;
+    static const VnlcPrimitiveType* DOUBLE_TYPE;
+    static const VnlcPrimitiveType* BOOLEAN_TYPE;
+    static const VnlcPrimitiveType* STRING_TYPE;
+
     VnlcPrimitiveType(bool readonly, VnlcPrimitiveTypeKind primitiveKind);
 
+public:
     [[nodiscard]] VnlcPrimitiveTypeKind getPrimitiveKind() const noexcept;
+
+    [[nodiscard]] static const VnlcPrimitiveType* getByteType();
+    [[nodiscard]] static const VnlcPrimitiveType* getShortType();
+    [[nodiscard]] static const VnlcPrimitiveType* getIntType();
+    [[nodiscard]] static const VnlcPrimitiveType* getLongType();
+    [[nodiscard]] static const VnlcPrimitiveType* getFloatType();
+    [[nodiscard]] static const VnlcPrimitiveType* getDoubleType();
+    [[nodiscard]] static const VnlcPrimitiveType* getBooleanType();
+    [[nodiscard]] static const VnlcPrimitiveType* getStringType();
 };
 
 #endif // VNLC_PRIMITIVE_TYPE_HPP
