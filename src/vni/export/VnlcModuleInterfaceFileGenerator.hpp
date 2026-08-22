cded73dd4a3d646a0b596c522ee40916cfc0c3d3
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-08-22 15:31:46 +0800

    feat: add vni file generator header file

---
+++ src/vni/export/VnlcModuleInterfaceFileGenerator.hpp
@@ -0,0 +1,14 @@
+#ifndef VNLC_MODULE_INTERFACE_FILE_GENERATOR_HPP
+#define VNLC_MODULE_INTERFACE_FILE_GENERATOR_HPP
+
+#include "../../ast/module/VnlcModuleNode.hpp"
+#include "../../config/VnlcConfig.hpp"
+#include <fstream>
+#include <string>
+#include <string_view>
+
+class VnlcModuleInterfaceFileGenerator {
+    
+};
+
+#endif // VNLC_MODULE_INTERFACE_FILE_GENERATOR_HPP
