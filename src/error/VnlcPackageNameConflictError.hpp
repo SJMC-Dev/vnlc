9f2405ee99166bb38bf346d92031a7ffefcc7e89
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-08-24 13:59:07 +0800

    fix: use fmt to keep consistency of string construction

--- src/error/VnlcPackageNameConflictError.hpp
+++ src/error/VnlcPackageNameConflictError.hpp
@@ -2,10 +2,11 @@
 #define VNLC_PACKAGE_NAME_CONFLICT_ERROR_HPP
 
 #include "VnlcError.hpp"
+#include <fmt/core.h>
 
 class VnlcPackageNameConflictError : public VnlcError {
 public:
-    VnlcPackageNameConflictError(std::string_view packageName) : VnlcError("Found two or more packages with the name: " + std::string(packageName)) {}
+    VnlcPackageNameConflictError(std::string_view packageName) : VnlcError(fmt::format("Found two or more packages with the name: {}", packageName)) {}
 };
 
 #endif // VNLC_PACKAGE_NAME_CONFLICT_ERROR_HPP
