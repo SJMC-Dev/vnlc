f4177ba29aa7198d03024caf75d81b065dc84b3e
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-08-24 16:23:59 +0800

    feat: add helper methods for module interface file reader

--- src/vni/import/VnlcModuleInterfaceFileReader.cpp
+++ src/vni/import/VnlcModuleInterfaceFileReader.cpp
@@ -1,8 +1,37 @@
 #include "VnlcModuleInterfaceFileReader.hpp"
+#include "../../error/VnlcModuleInterfaceReaderError.hpp"
+#include <filesystem>
+#include <fmt/core.h>
+#include <fstream>
 
-VnlcModuleInterfaceFileReader::VnlcModuleInterfaceFileReader(std::istream& source) : source(source) {}
+VnlcModuleInterfaceFileReader::VnlcModuleInterfaceFileReader(std::filesystem::path filePath) : filePath(std::filesystem::canonical(filePath)) {}
 
-std::unique_ptr<VnlcImportedItem> VnlcModuleInterfaceFileReader::read() {
+std::unique_ptr<VnlcImportedModule> VnlcModuleInterfaceFileReader::read() {
+    if (!std::filesystem::exists(filePath)) {
+        throw VnlcModuleInterfaceFileReaderError(fmt::format("File {} does not exist", filePath.string()));
+    } else if (!std::filesystem::is_regular_file(filePath)) {
+        throw VnlcModuleInterfaceFileReaderError(fmt::format("Path {} is not a regular file", filePath.string()));
+    } else if (std::filesystem::is_empty(filePath)) {
+        throw VnlcModuleInterfaceFileReaderError(fmt::format("File {} is empty", filePath.string()));
+    } else if (std::filesystem::is_directory(filePath)) {
+        throw VnlcModuleInterfaceFileReaderError(fmt::format("Path {} is a directory, not a file", filePath.string()));
+    } else if (!std::filesystem::is_regular_file(filePath)) {
+        throw VnlcModuleInterfaceFileReaderError(fmt::format("Path {} is not a regular file", filePath.string()));
+    } else if (filePath.extension() != ".vni") {
+        throw VnlcModuleInterfaceFileReaderError(fmt::format("File {} must be a .vni file", filePath.string()));
+    }
+
     nlohmann::json json;
-    source >> json;
+    std::ifstream file(filePath);
+
+    try {
+        json = nlohmann::json::parse(file);
+    } catch (const nlohmann::json::parse_error& e) {
+        throw VnlcModuleInterfaceFileReaderError(e.what());
+    }
+
+    for (auto& [key, value] : json.items()) {
+    }
+
+    throw VnlcModuleInterfaceFileReaderError(fmt::format("Not implemented"));
 }
--- src/vni/import/VnlcModuleInterfaceFileReader.hpp
+++ src/vni/import/VnlcModuleInterfaceFileReader.hpp
@@ -1,19 +1,46 @@
 #ifndef VNLC_MODULE_INTERFACE_FILE_READER_HPP
 #define VNLC_MODULE_INTERFACE_FILE_READER_HPP
 
-#include "VnlcImportedItem.hpp"
-#include <istream>
+#include "VnlcImportedAlias.hpp"
+#include "VnlcImportedClass.hpp"
+#include "VnlcImportedConst.hpp"
+#include "VnlcImportedEnum.hpp"
+#include "VnlcImportedEnumMember.hpp"
+#include "VnlcImportedFunc.hpp"
+#include "VnlcImportedInterface.hpp"
+#include "VnlcImportedModule.hpp"
+#include "VnlcImportedLet.hpp"
+#include "VnlcImportedMethod.hpp"
+#include "VnlcImportedParameter.hpp"
+#include "VnlcImportedProperty.hpp"
+#include "VnlcImportedTypeAlias.hpp"
+#include "VnlcImportedVar.hpp"
+#include <filesystem>
 #include <memory>
 #include <nlohmann/json.hpp>
 
 class VnlcModuleInterfaceFileReader {
 private:
-    std::istream& source;
+    std::filesystem::path filePath;
+
+    std::unique_ptr<VnlcImportedVar> parseImportedVar(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedLet> parseImportedLet(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedConst> parseImportedConst(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedFunc> parseImportedFunc(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedMethod> parseImportedMethod(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedClass> parseImportedClass(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedInterface> parseImportedInterface(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedEnum> parseImportedEnum(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedEnumMember> parseImportedEnumMember(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedTypeAlias> parseImportedTypeAlias(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedAlias> parseImportedAlias(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedProperty> parseImportedProperty(const nlohmann::json& json);
+    std::unique_ptr<VnlcImportedParameter> parseImportedParameter(const nlohmann::json& json);
 
 public:
-    VnlcModuleInterfaceFileReader(std::istream& source);
+    VnlcModuleInterfaceFileReader(std::filesystem::path filePath);
 
-    [[nodiscard]] std::unique_ptr<VnlcImportedItem> read();
+    [[nodiscard]] std::unique_ptr<VnlcImportedModule> read();
 };
 
 #endif // VNLC_MODULE_INTERFACE_FILE_READER_HPP
