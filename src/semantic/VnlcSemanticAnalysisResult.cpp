16cf57a846e7124045e462e2514cbeaad875f62b
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-09-01 15:41:12 +0800

    fix: redefine the constructor of semantic analysis result and add imported packages field with its getter

--- src/semantic/VnlcSemanticAnalysisResult.cpp
+++ src/semantic/VnlcSemanticAnalysisResult.cpp
@@ -1,10 +1,20 @@
 #include "VnlcSemanticAnalysisResult.hpp"
 #include <optional>
 
-VnlcSemanticAnalysisResult::VnlcSemanticAnalysisResult(std::vector<VnlcDiagnostic>&& errors, std::vector<VnlcDiagnostic>&& warnings, std::vector<VnlcDiagnostic>&& notes)
+VnlcSemanticAnalysisResult::VnlcSemanticAnalysisResult(
+    std::vector<VnlcDiagnostic>&& errors,
+    std::vector<VnlcDiagnostic>&& warnings,
+    std::vector<VnlcDiagnostic>&& notes,
+    std::unordered_set<std::unique_ptr<VnlcReferenceType>>&& referenceTypes,
+    std::unordered_map<const VnlcTypeNode*, const VnlcSemanticType*>&& semanticTypeMap,
+    std::unordered_map<std::string, std::unique_ptr<VnlcImportedPackage>>&& importedPackages
+)
     : errors(std::move(errors)),
       warnings(std::move(warnings)),
-      notes(std::move(notes)) {}
+      notes(std::move(notes)),
+      referenceTypes(std::move(referenceTypes)),
+      semanticTypeMap(std::move(semanticTypeMap)),
+      importedPackages(std::move(importedPackages)) {}
 
 bool VnlcSemanticAnalysisResult::hasErrors() const {
     return !errors.empty();
@@ -34,6 +44,14 @@ const std::optional<const VnlcSemanticType*> VnlcSemanticAnalysisResult::getSema
     auto it = semanticTypeMap.find(typeNode);
     if (it != semanticTypeMap.end()) {
         return std::make_optional<const VnlcSemanticType*>(it->second);
+    }
+    return std::nullopt;
+}
+
+const std::optional<const VnlcImportedPackage*> VnlcSemanticAnalysisResult::getImportedPackageByName(std::string_view packageName) const {
+    auto it = importedPackages.find(std::string(packageName));
+    if (it != importedPackages.end()) {
+        return std::make_optional<const VnlcImportedPackage*>(it->second.get());
     }
     return std::nullopt;
 }
--- src/semantic/VnlcSemanticAnalysisResult.hpp
+++ src/semantic/VnlcSemanticAnalysisResult.hpp
@@ -3,8 +3,10 @@
 
 #include "../ast/type/VnlcTypeNode.hpp"
 #include "../diagnostic/VnlcDiagnostic.hpp"
+#include "../vni/import/VnlcImportedPackage.hpp"
 #include "type/VnlcReferenceType.hpp"
 #include "type/VnlcSemanticType.hpp"
+#include <memory>
 #include <optional>
 #include <unordered_map>
 #include <unordered_set>
@@ -19,8 +21,17 @@ private:
     std::unordered_set<std::unique_ptr<VnlcReferenceType>> referenceTypes;
     std::unordered_map<const VnlcTypeNode*, const VnlcSemanticType*> semanticTypeMap;
 
+    std::unordered_map<std::string, std::unique_ptr<VnlcImportedPackage>> importedPackages;
+
 public:
-    VnlcSemanticAnalysisResult(std::vector<VnlcDiagnostic>&& errors, std::vector<VnlcDiagnostic>&& warnings, std::vector<VnlcDiagnostic>&& notes);
+    VnlcSemanticAnalysisResult(
+        std::vector<VnlcDiagnostic>&& errors,
+        std::vector<VnlcDiagnostic>&& warnings,
+        std::vector<VnlcDiagnostic>&& notes,
+        std::unordered_set<std::unique_ptr<VnlcReferenceType>>&& referenceTypes,
+        std::unordered_map<const VnlcTypeNode*, const VnlcSemanticType*>&& semanticTypeMap,
+        std::unordered_map<std::string, std::unique_ptr<VnlcImportedPackage>>&& importedPackages
+    );
     VnlcSemanticAnalysisResult(const VnlcSemanticAnalysisResult&) = default;
     VnlcSemanticAnalysisResult& operator=(const VnlcSemanticAnalysisResult&) = default;
     VnlcSemanticAnalysisResult(VnlcSemanticAnalysisResult&&) noexcept = default;
@@ -34,6 +45,7 @@ public:
     [[nodiscard]] const std::vector<VnlcDiagnostic>& getNotes() const;
 
     [[nodiscard]] const std::optional<const VnlcSemanticType*> getSemanticTypeByTypeNode(const VnlcTypeNode* typeNode) const;
+    [[nodiscard]] const std::optional<const VnlcImportedPackage*> getImportedPackageByName(std::string_view packageName) const;
 };
 
 #endif // VNLC_SEMANTIC_ANALYSIS_RESULT_HPP
