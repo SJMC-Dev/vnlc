b5bbe8067e78596decde153d272f49ac73f38348
Renvle RS <47477363+renvlers@users.noreply.github.com>
2026-09-04 09:06:17 +0800

    test(parser): cover loop variable declaration kind

--- tests/VnlcParserTest.cpp
+++ tests/VnlcParserTest.cpp
@@ -1,8 +1,12 @@
-#include "../src/parser/VnlcParser.hpp"
+#include "../src/ast/declaration/VnlcFunctionDeclarationNode.hpp"
+#include "../src/ast/statement/VnlcForStatementNode.hpp"
+#include "../src/ast/statement/VnlcVariableDeclarationStatementNode.hpp"
 #include "../src/config/VnlcConfig.hpp"
+#include "../src/parser/VnlcParser.hpp"
 #include <filesystem>
-#include <gtest/gtest.h>
 #include <fstream>
+#include <gtest/gtest.h>
+#include <sstream>
 
 TEST(VnlcParserTest, SimpleModule) {
     std::ifstream input("assets/demo/main.vnl");
@@ -21,4 +25,41 @@ TEST(VnlcParserTest, SimpleModule) {
     };
 
     auto result = parser.parse(std::move(config));
+}
+
+TEST(VnlcParserTest, DistinguishesLoopVariablesFromOrdinaryVariables) {
+    std::stringstream input("func test() {\n    let value = 1\n    for (let item in 1..3) {}\n}\n");
+    VnlcLexer lexer(input);
+    VnlcParser parser(std::move(lexer));
+
+    VnlcConfig config{
+        .mode = VnlcRunningMode::COMPILE,
+        .vanillangVersion = "1.0",
+        .minecraftVersion = "26.1.2",
+        .packageRootPath = std::filesystem::current_path(),
+        .inputFilePath = std::filesystem::current_path() / "test.vnl",
+        .outputDirectory = std::nullopt,
+        .dependencyPackageRootPaths = {},
+        .optimizationLevel = std::nullopt,
+    };
+
+    auto module = parser.parse(config);
+    ASSERT_EQ(module->getTopIdentifierDeclarations().size(), 1);
+
+    const auto* function = dynamic_cast<const VnlcFunctionDeclarationNode*>(module->getTopIdentifierDeclarations().front().get());
+    ASSERT_NE(function, nullptr);
+    ASSERT_TRUE(function->getBody().has_value());
+
+    const auto& statements = function->getBody().value()->getStatements();
+    ASSERT_EQ(statements.size(), 2);
+
+    const auto* variableStatement = dynamic_cast<const VnlcVariableDeclarationStatementNode*>(statements[0].get());
+    ASSERT_NE(variableStatement, nullptr);
+    EXPECT_EQ(variableStatement->getVariableDeclaration().getKind(), VnlcValueDeclarationType::Kind::LET);
+    EXPECT_TRUE(variableStatement->getVariableDeclaration().getInitializer().has_value());
+
+    const auto* forStatement = dynamic_cast<const VnlcForStatementNode*>(statements[1].get());
+    ASSERT_NE(forStatement, nullptr);
+    EXPECT_EQ(forStatement->getLoopVariable().getKind(), VnlcValueDeclarationType::Kind::LOOP_VARIABLE);
+    EXPECT_FALSE(forStatement->getLoopVariable().getInitializer().has_value());
 }
